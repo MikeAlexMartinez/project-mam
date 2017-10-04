4d87100bc093f6c62233482e2ca5bd91821cac79
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-10-04 21:23:50 +0100

    added componentDidMount Fetch function to Gallery

--- src/components/ui/Admin/Gallery.js
+++ src/components/ui/Admin/Gallery.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { CloudinaryContext, Image } from 'cloudinary-react';
 import 'whatwg-fetch';
+
+const imageStartUrl = 'http://res.cloudinary.com/di6bv5utg/image/upload/v1506681645/';
+const cloudName = 'di6bv5utg';
+
+function ProjectPhoto({ url }) {
+  return (
+    <CloudinaryContext cloudName={cloudName} >
+      <div className="responsive" >
+        <div className="img">
+          <a target="_blank" href={`${imageStartUrl + url}.jpg`}>
+            <Image publicId={url} />
+          </a>
+        </div>
+      </div>
+    </CloudinaryContext>
+  );
+}
+
+ProjectPhoto.propTypes = {
+  url: PropTypes.string.isRequired,
+};
 
 class Gallery extends Component {
   constructor(props) {
@@ -9,8 +32,31 @@ class Gallery extends Component {
     };
   }
 
-  /* componentDidMount(){
-  } */
+  componentDidMount() {
+    function checkStatus(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return response;
+      }
+
+      const error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+
+    function parseJSON(response) {
+      return response.json();
+    }
+
+    fetch('http://res.cloudinary.com/di6bv5utg/image/list/project-mam.json')
+      .then(checkStatus)
+      .then(parseJSON)
+      .then((response) => {
+        this.setState({ gallery: response.resources });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   render() {
     const { gallery } = this.state;
@@ -18,7 +64,12 @@ class Gallery extends Component {
       <div>
         <h1>Image Gallery</h1>
         {gallery.length !== 0 ?
-          <div>Images Found!</div> :
+          gallery.map(({ public_id }, i) => {
+            const id = `image_${i}`;
+            return (
+              <ProjectPhoto url={public_id} key={id} />
+            );
+          }) :
           <div>No Images found (WAH)</div>
         }
       </div>
---
+++ src/components/ui/PictureCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Button from 'material-ui/Typography';
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 200,
+  },
+};
+
+function PictureCard(props) {
+  const classes = props.classes;
+}
